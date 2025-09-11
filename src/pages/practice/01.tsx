4c22fabf87d4f4409180b889bede13379a890518
Mayu Adachi <ykmshiss@gmail.com>
2025-09-11 22:03:48 +0900

    propsで内容を渡す

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -6,7 +6,15 @@ import BoxContent from "@/components/BoxContent";
 const Page: NextPage = () => {
   return(
 <div className="mx-auto mt-10 max-w-4xl">
-  <BoxContent />
+  <BoxContent
+  title1="これはタイトルです"
+  content1="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。" 
+  title2="これはタイトルです"
+  content2="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。" 
+  title3="これはタイトルです"
+  content3="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。" 
+  />
+
 </div>
 );
 };
